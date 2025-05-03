ee9146be580c19f972296fe34e18c41227c7073f
David-webspace <web.affabledavid5288@gmail.com>
2025-05-03 22:45:45 +0800

    opening css update

--- src/components/Opening.jsx
+++ src/components/Opening.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { FaChevronDown } from "react-icons/fa";
-import '../css/Opening.css'
+import '../css/opening.css'
 import { Link } from 'react-router-dom';
 
 const Opening = () => {
